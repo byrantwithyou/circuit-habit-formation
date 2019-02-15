776ca9c311a0085a6df0ea94b3481026c714ea9b
byrantwithyou <byrantwithyou@163.com>
2019-02-15 16:08:20 -0500

    add vue-video-player

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <v-app>
-    <v-footer app>
-      <v-spacer></v-spacer>
-      <v-btn>Next</v-btn>
-    </v-footer>
+    <v-content>
+      <v-container>
+        <v-flex>
+          <v-card>
+            <v-card-actions>
+              <v-btn>snapshot</v-btn>
+            </v-card-actions>
+            <v-card-media>
+              
+            </v-card-media>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data () {
     return {
-      //
+    }
+  },
+  methods: {
+    snapshot: function() {
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Notifications from "vue-notification"
+import VueVideoPlayer from "vue-video-player"
+import "video.js/dist/video-js.css"
 
 Vue.config.productionTip = false
-
+Vue.use(Notifications)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
